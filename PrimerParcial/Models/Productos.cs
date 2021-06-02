85034e128d86b1b5c0c5879b5cf6f1ff9ad53df8
Albenny Hernandez Cruz <albenny_hernandez@ucne.edu.do>
2021-06-02 19:11:25 -0400

    realice la clase

--- PrimerParcial/Models/Productos.cs
+++ PrimerParcial/Models/Productos.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PrimerParcial.Models
 {
     public class Productos
-    {
+    { 
+        
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set;}
+        public int Existencia { get; set; }
+        public double Costo { get; set;}
+        public double ValorInventario { get; set;}
+
     }
+
 }
