c6565d528de75ba325fc4028d0215218c110f4ca
Albenny Hernandez Cruz <albenny_hernandez@ucne.edu.do>
2021-06-02 21:23:22 -0400

    hice la migracion

---
+++ PrimerParcial/Migrations/20210603012237_migracion_inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimerParcial.Migrations
+{
+    public partial class migracion_inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Existencia = table.Column<float>(type: "REAL", nullable: false),
+                    Costo = table.Column<float>(type: "REAL", nullable: false),
+                    ValorInventario = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_productos", x => x.ProductoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "productos");
+        }
+    }
+}
